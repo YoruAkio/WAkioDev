583804f52dbf6eab63f9668bd970cfd85261e250
YoruAkio <shahrimalik1124@gmail.com>
2022-10-02 19:38:47 +0700

    Fixing GET routes link

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -12,16 +12,16 @@ const Main = ({ children, router }) => {
         <meta name="description" content="Akio's homepage" />
         <meta name="author" content="Yoru Akio" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
         <meta name="twitter:title" content="Yoru Akio" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@yoruakio" />
         <meta name="twitter:creator" content="@yoruakio" />
-        <meta name="twitter:image" content="https://www.akio-is-dev.ninja/card.png" />
+        <meta name="twitter:image" content="https://akio-dev.ml/card.png" />
         <meta property="og:site_name" content="Yoru Akio" />
         <meta name="og:title" content="Yoru Akio" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.akio-is-dev.ninja/card.png" />
+        <meta property="og:image" content="https://akio-dev.ml/card.png" />
         <title>YoruAkio - Homepage</title>
       </Head>
 
